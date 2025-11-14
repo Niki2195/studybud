e0f96b7003fc0ec561a8b1007af184a2db668554
Niki2195 <97636288+Niki2195@users.noreply.github.com>
2025-11-14 16:45:26 +0400

    Refactor import statement for base.routing

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -2,7 +2,7 @@ import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
-import base.routing  # Если у тебя есть routing.py в приложении base
+import base.routing  
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
@@ -10,7 +10,7 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            base.routing.websocket_urlpatterns  # нужно создать в base/routing.py
+            base.routing.websocket_urlpatterns 
         )
     ),
 })
